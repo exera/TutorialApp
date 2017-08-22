40c97f4b5b4d3ad03dc6ca51261c525658e7fe24
ahmet.sancar <sancarahm3t@hotmail.com>
2017-08-22 15:52:16 +0300

    initial

---
+++ Web/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            var images = new List<Image>{
+                new Image {
+                    Url = "/contents/images/image_1068.jpg",
+                    Alt = "1068"
+                },
+                new Image {
+                    Url = "/contents/images/image_1070.jpg",
+                    Alt = "1070"
+                },
+            };
+
+            Image foundImage = null;
+            foreach (var image in images)
+            {
+                if(image.Alt == "1068")
+                {
+                    foundImage = image;
+                    break;
+                }
+            }
+
+
+
+            return View(images);
+        }
+
+        public ActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ Web/Controllers/SqlController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web.Controllers
+{
+    //
+    public class SqlController : Controller
+    {
+        // GET: Sql
+        public string Index()
+        {
+            var connStr = ConfigurationManager.ConnectionStrings["default"].ConnectionString;
+            var conn = new SqlConnection(connStr);
+            conn.Open();
+
+            var command = new SqlCommand("select * from Employee",conn);
+            var dataReader = command.ExecuteReader();
+            var result = "";
+            while (dataReader.Read())
+            {
+                var id = (int)dataReader["Id"];
+                var name = (string)dataReader["Name"];
+                var age = (int)dataReader["Age"];
+
+                result += id + ", " + name + ", " + age + "<br>";
+            }
+
+            return result;
+        }
+    }
+}
